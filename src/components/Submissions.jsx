68107dcdd910ac2d6d3b7649f936d17e8fd48f42
YourGitHubUsername <your_github_email@example.com>
2025-02-06 16:05:55 -0500

    changed submissions copy

--- src/components/Submissions.jsx
+++ src/components/Submissions.jsx
@@ -7,12 +7,15 @@ const Submissions = () => {
       <div className="text-center">
         <div className="space-y-8 text-center">
           <div>
-            <h2 className="text-2xl mb-1">guidelines</h2>
-            <p className="mb-2">
+            <p className="mb-5">
+              For our first issue, we're looking for work that explores remnants, remainders, and reminders. Is a coffee ring just a stain when it outlives the mug? Does a footprint exist? Give us anything!
+            </p>
+            <h2 className="text-2xl mb-2">guidelines</h2>
+            <p className="mb-1">
               Our submissions are open to anyone and everyone. We accept poetry and fiction, as well as essays on 
               literary themes. You may submit up to five poems. Prose can be any length, although an excerpt of up to 
-              1000 words should be underlined as the section you would most prefer to be published. Multiple poems should 
-              be submitted in a single pdf.
+              1000 words should be underlined as the section you would most prefer to be published. Regardless of length, your submission should 
+              be sent in a single pdf.
               </p>
               <p>
               We accept submissions by email at <a 
@@ -21,8 +24,7 @@ const Submissions = () => {
               >
                 submissions@lunchbreakreview.com
               </a>. Our editors are 
-              happy to provide feedback on submissions. Please let us know whether you would like feedback with your 
-              submission.
+              happy to provide feedback on submissions; please let us know if you would like it.
             </p>
           </div>
 
