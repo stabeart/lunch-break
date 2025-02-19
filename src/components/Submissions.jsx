e0719f0649ee334f3339e81f13922525e0908444
YourGitHubUsername <your_github_email@example.com>
2025-02-19 14:11:10 -0500

    added visual submissions copy

--- src/components/Submissions.jsx
+++ src/components/Submissions.jsx
@@ -7,14 +7,19 @@ const Submissions = () => {
       <div className="text-center">
         <div className="space-y-8 text-center">
           <div>
-            <p className="mb-5">
+            <p className="mb-10">
               For our first issue, we're looking for work that explores remnants, remainders, and reminders. Is a coffee ring just a stain when it outlives the mug? Does a footprint exist? Give us anything!
             </p>
-            <h2 className="text-2xl mb-2">guidelines</h2>
-            <p className="mb-1">
-              Our submissions are open to anyone and everyone. We accept poetry and fiction, as well as essays on 
-              literary themes. You may submit up to five poems. Prose can be any length, although an excerpt of up to 
-              1000 words should be underlined as the section you would most prefer to be published. Regardless of length, your submission should 
+            <h2 className="text-2xl mb-3">guidelines</h2>
+            <p className="mb-3">
+              Our submissions are open to anyone and everyone. We accept poetry, fiction, essays, and visual art.
+              </p>
+              <p className="mb-3">
+              You may submit up to five poems. Prose can be any length, although an excerpt of up to 
+              1000 words should be underlined as the section you would most prefer to be published. 
+              </p>
+              <p className="mb-3">
+              Regardless of length, your submission should 
               be sent in a single pdf.
               </p>
               <p>
@@ -23,8 +28,8 @@ const Submissions = () => {
                 className="hover:opacity-30 transition-opacity underline"
               >
                 submissions@lunchbreakreview.com
-              </a>. Our editors are 
-              happy to provide feedback on submissions; please let us know if you would like it.
+              </a>. We're
+              happy to provide feedback on written submissions; please let us know if you would like it.
             </p>
           </div>
 
