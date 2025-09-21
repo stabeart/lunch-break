01e0ec995cdefa8287f25f99f2c719bac4649539
YourGitHubUsername <your_github_email@example.com>
2025-09-21 00:32:44 -0400

    removed paylink

--- src/components/DonatePage.jsx
+++ src/components/DonatePage.jsx
@@ -7,25 +7,13 @@ const DonatePage = () => {
       <div className="text-center">
         <div className="space-y-8">
           <p className="mb-6 text-xl">
-            We are a fledgling, not-for-profit magazine run by two college students. 
+            We are a fledgling, not-for-profit magazine run by a handful of college students. 
             Your generous donation will go towards maintaining our website and covering 
             the cost of printing.
           </p>
 
           <div className="space-y-6">
             <div className="flex flex-col items-center gap-4">
-              <a
-                href="https://pay.lunchbreakreview.com"
-                className="px-8 py-3 bg-[#2E6F40] text-white rounded-md hover:bg-opacity-90 transition-opacity w-full max-w-xs"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Our Pay Link
-              </a>
-              <p classname="mb-6 text-xl">
-                Or
-              </p>
-              
               <a 
                 href="https://paypal.me/lunchbreakrvw"
                 className="px-8 py-3 bg-[#0070BA] text-white rounded-md hover:bg-opacity-90 transition-opacity w-full max-w-xs"
